45b52f6c487e4922cb7a6d4418d435ea033eff4b
Evan Pratten <ewpratten@gmail.com>
2021-07-29 18:20:30 -0400

    Disable tapdance layers

--- keyboards/tg4x/keymaps/ewpratten/keymap.c
+++ keyboards/tg4x/keymaps/ewpratten/keymap.c
@@ -17,16 +17,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_BASE] = LAYOUT(
         KC_ESC,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,  KC_P, KC_DEL, KC_BSPC,
         KC_TAB,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, LT(LAYER_LEDS, KC_L), KC_SCLN, KC_ENT,
-        KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, LT(LAYER_NAVI, KC_N), KC_M, KC_COMM, KC_DOT, KC_RSPC, TT(LAYER_SYMB),
-        KC_LCTL, KC_LALT, KC_LGUI, KC_SPACE, KC_SPACE, TT(LAYER_EXTRA), _______, _______, TO(LAYER_COLEMAK)
+        KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, LT(LAYER_NAVI, KC_N), KC_M, KC_COMM, KC_DOT, KC_RSPC, MO(LAYER_SYMB),
+        KC_LCTL, KC_LALT, KC_LGUI, KC_SPACE, KC_SPACE, MO(LAYER_EXTRA), _______, _______, TO(LAYER_COLEMAK)
     ),
 
     // QWERTY layer
     [LAYER_COLEMAK] = LAYOUT(
         KC_ESC,  KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, LT(LAYER_LEDS, KC_L), KC_U, KC_Y,  KC_SCLN, _______, KC_BSPC,
         KC_TAB,  KC_A, KC_R, KC_S, KC_T, KC_D, KC_H, LT(LAYER_NAVI, KC_N), KC_E, KC_I, KC_O, KC_ENT,
-        KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_K, KC_M, KC_COMM, KC_DOT, KC_RSPC, TT(LAYER_SYMB),
-        KC_LCTL, KC_LALT, KC_LGUI, KC_SPACE, KC_SPACE, TT(LAYER_EXTRA), _______, _______, TO(LAYER_BASE)
+        KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_K, KC_M, KC_COMM, KC_DOT, KC_RSPC, MO(LAYER_SYMB),
+        KC_LCTL, KC_LALT, KC_LGUI, KC_SPACE, KC_SPACE, MO(LAYER_EXTRA), _______, _______, TO(LAYER_BASE)
     ),
 
     // Number and Symbol layer
