cdd6db3049b62679a1a93766f86afab9fc918818
Evan Pratten <ewpratten@gmail.com>
2022-02-25 14:07:38 -0500

    Add support for one-shot macro keys

--- keyboards/ferris/keymaps/ewpratten/keymap.c
+++ keyboards/ferris/keymaps/ewpratten/keymap.c
@@ -5,7 +5,15 @@ enum ferris_layers {
     _COLEMAK,  // Base Layer
     _NSYM,     // Numbers and Symbols
     _NAVIGATION,  // Navigation layer
+    _SHORTCUT, // Shortcut Layer
 };
+
+// Shorthands
+#define GO_HOME TO(_COLEMAK)
+#define GO_NAV TO(_NAVIGATION)
+#define GO_SHRT OSL(_SHORTCUT)
+#define LD_TERM LGUI(KC_ENT)
+#define CC_QUIT LGUI(LSFT(KC_Q))
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -17,17 +25,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_NSYM] = LAYOUT(
-    KC_1, KC_2, KC_3, KC_EQL,  TO(_NAVIGATION),       KC_GESC,  KC_NO,   KC_NO,   KC_DEL,    KC_SCLN,
-    KC_4, KC_5, KC_6, KC_0,    KC_TAB,                KC_END,    KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT,
-    KC_7, KC_8, KC_9, KC_MINS, KC_HOME,               KC_QUOTE, KC_SLSH, KC_LBRC, KC_RBRC,  KC_BSLS,
-                     KC_LSFT, KC_LCTL,                KC_NO,  KC_LGUI
+    KC_1, KC_2, KC_3, KC_EQL,  GO_NAV,        KC_GESC,  KC_NO,   GO_SHRT,   KC_DEL,    KC_SCLN,
+    KC_4, KC_5, KC_6, KC_0,    KC_TAB,        KC_END,   KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT,
+    KC_7, KC_8, KC_9, KC_MINS, KC_HOME,       KC_QUOTE, KC_SLSH, KC_LBRC, KC_RBRC,  KC_BSLS,
+                      KC_LSFT, KC_LCTL,       KC_NO,    KC_LGUI
   ),
 
   [_NAVIGATION] = LAYOUT(
     KC_NO,   KC__VOLDOWN, KC__VOLUP, KC_PSCR, KC_NO,              KC_NO,   KC_NO,   KC_PGDN, KC_PGUP, KC_NO,
     KC_MPRV, KC_MPLY,     KC_MNXT,   KC_NO,   KC_NO,              KC_NO,   KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
-    KC_LALT, KC_NO,       KC_NO,     KC_NO,   TO(_COLEMAK),       KC_NO,   KC_NO,   KC_WH_D, KC_WH_U, KC_NO,
-                          KC_LSFT, KC_LCTL,                       KC_BTN1, KC_BTN2
+    KC_LALT, KC_NO,       KC_NO,     KC_NO,   GO_HOME,            KC_NO,   KC_NO,   KC_WH_D, KC_WH_U, KC_NO,
+                          KC_LSFT,   KC_LCTL,                     KC_BTN1, KC_BTN2
+  ),
+
+  [_SHORTCUT] = LAYOUT(
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        LD_TERM, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        CC_QUIT, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, GO_HOME,      KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,
+                         KC_NO, KC_NO,        KC_NO,   KC_NO
   )
 };
 // clang-format on
