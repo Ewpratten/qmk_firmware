f118e99711bebf7e0b5adf2a0e17009f05180cd5
Evan Pratten <ewpratten@gmail.com>
2022-02-25 09:14:44 -0500

    move reset

--- keyboards/ferris/keymaps/ewpratten/keymap.c
+++ keyboards/ferris/keymaps/ewpratten/keymap.c
@@ -17,14 +17,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_NSYM] = LAYOUT(
-    KC_1, KC_2, KC_3, KC_EQL,  TO(_NAVIGATION),       KC_GESC,  KC_NO,   RESET,   KC_DEL,    KC_SCLN,
+    KC_1, KC_2, KC_3, KC_EQL,  TO(_NAVIGATION),       KC_GESC,  KC_NO,   KC_NO,   KC_DEL,    KC_SCLN,
     KC_4, KC_5, KC_6, KC_0,    KC_TAB,                KC_END,    KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT,
     KC_7, KC_8, KC_9, KC_MINS, KC_HOME,               KC_QUOTE, KC_SLSH, KC_LBRC, KC_RBRC,  KC_BSLS,
                      KC_LSFT, KC_LCTL,                KC_NO,  KC_LGUI
   ),
 
   [_NAVIGATION] = LAYOUT(
-    KC_NO,   KC__VOLDOWN, KC__VOLUP, KC_PSCR, KC_NO,              KC_NO,   KC_NO,   KC_PGDN, KC_PGUP, KC_NO,
+    KC_NO,   KC__VOLDOWN, KC__VOLUP, KC_PSCR, KC_NO,              RESET,   KC_NO,   KC_PGDN, KC_PGUP, KC_NO,
     KC_MPRV, KC_MPLY,     KC_MNXT,   KC_NO,   KC_NO,              KC_NO,   KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
     KC_LALT, KC_NO,       KC_NO,     KC_NO,   TO(_COLEMAK),       KC_NO,   KC_NO,   KC_WH_D, KC_WH_U, KC_NO,
                           KC_LSFT, KC_LCTL,                       KC_BTN1, KC_BTN2
