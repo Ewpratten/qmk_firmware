574254550bfe2d0b60ce2b081c32f70803827e85
Evan Pratten <ewpratten@gmail.com>
2022-03-09 16:18:55 -0500

    switch to colemak dh

--- keyboards/ferris/keymaps/ewpratten/keymap.c
+++ keyboards/ferris/keymaps/ewpratten/keymap.c
@@ -19,9 +19,9 @@ enum ferris_layers {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT(
-    KC_Q, KC_W,    KC_F,    KC_P,    KC_G,            KC_J,      KC_L,  KC_U,    KC_Y,    KC_BSPC,
-    KC_A, KC_R,    KC_S,    KC_T,    KC_D,            KC_H,      KC_N,  KC_E,    KC_I,    KC_O,
-    KC_Z, KC_X,    KC_C,    KC_V,    KC_B,            KC_K,      KC_M,  KC_COMM, KC_DOT, KC_ENT,
+    KC_Q, KC_W,    KC_F,    KC_P,    KC_B,            KC_J,      KC_L,  KC_U,    KC_Y,    KC_BSPC,
+    KC_A, KC_R,    KC_S,    KC_T,    KC_G,            KC_M,      KC_N,  KC_E,    KC_I,    KC_O,
+    KC_Z, KC_X,    KC_C,    KC_D,    KC_V,            KC_K,      KC_H,  KC_COMM, KC_DOT, KC_ENT,
                             KC_LSPO, KC_LCPO,         MO(_NSYM), KC_SPACE
   ),
 
