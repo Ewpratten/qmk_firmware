1c3040c17fe231fa6557da8ef6c228170d4fdd3c
Evan Pratten <ewpratten@gmail.com>
2022-03-09 16:38:16 -0500

    Cursed colemak

--- keyboards/ferris/keymaps/ewpratten/keymap.c
+++ keyboards/ferris/keymaps/ewpratten/keymap.c
@@ -20,8 +20,8 @@ enum ferris_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT(
     KC_Q, KC_W,    KC_F,    KC_P,    KC_B,            KC_J,      KC_L,  KC_U,    KC_Y,    KC_BSPC,
-    KC_A, KC_R,    KC_S,    KC_T,    KC_G,            KC_M,      KC_N,  KC_E,    KC_I,    KC_O,
-    KC_Z, KC_X,    KC_C,    KC_D,    KC_V,            KC_K,      KC_H,  KC_COMM, KC_DOT, KC_ENT,
+    KC_A, KC_R,    KC_S,    KC_T,    KC_G,            KC_H,      KC_N,  KC_E,    KC_I,    KC_O,
+    KC_Z, KC_X,    KC_C,    KC_D,    KC_V,            KC_K,      KC_M,  KC_COMM, KC_DOT, KC_ENT,
                             KC_LSPO, KC_LCPO,         MO(_NSYM), KC_SPACE
   ),
 
