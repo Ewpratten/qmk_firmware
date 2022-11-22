8bd7e10797452d7342a39068cdc217d8bf32c286
Evan Pratten <ewpratten@gmail.com>
2022-11-22 18:19:39 -0500

    add a spacebar

--- keyboards/tg4x/keymaps/ewpratten/keymap.c
+++ keyboards/tg4x/keymaps/ewpratten/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
        KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_QUOTE, KC_SLSH, KC_LBRC, KC_RBRC, KC_BSLS, KC_F11, KC_NO,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_SPACE, KC_SPACE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
 
     // Extras layer
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC__VOLDOWN, KC__VOLUP, KC__MUTE, RESET, KC_NO, KC_NO, KC_PGUP, KC_NO,   KC_PGDN, KC_PSCR, KC_SLCK, KC_PAUS,
         KC_CAPS, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,KC_INS,  KC_NO,
         KC_TRNS, RGB_TOG, KC_NO, KC_NO, KC_NO, KC_HOME, KC_END,  TO(LAY_COLEMAK), KC_NO, KC_NO, KC_TRNS, KC_NO,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_SPACE, KC_SPACE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
 };
 // clang-format on
